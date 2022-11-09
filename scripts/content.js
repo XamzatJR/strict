417da10304edd9ecdd0accde40ae4f39d4ea8a91
XamzatJR <bahaev.hamzat95@gmail.com>
2022-11-09 21:39:50 +0300

    feat: add blockPage function and msg handler

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,16 @@
+const POMO = 'https://khamzat-pomodoro.netlify.app/';
+const url = window.location.href;
 
+function blockPage(blackList) {
+  blackList.forEach((el) => {
+    if (url.match(el)) {
+      window.location.href = POMO;
+    }
+  });
+}
+
+chrome.runtime.onMessage.addListener(function (request) {
+  if (request.isActive) {
+    blockPage();
+  }
+});
