d2bfb3d02dcf934b3e310c231a5c0481bfead332
XamzatJR <bahaev.hamzat95@gmail.com>
2022-11-09 21:34:44 +0300

    feat: add logic for passing msg to the content script when toggling the switcher

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,13 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#switcher').addEventListener('change', (event) => {
+    if (event.target.checked) {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { isActive: true });
+      });
+    } else {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { isActive: false });
+      });
+    }
+  });
+});
