b7a37918a223c4df6b7dd14b632a9990fe45e40f
XamzatJR <bahaev.hamzat95@gmail.com>
2022-12-04 22:22:25 +0300

    feat: add functions to delete and add list items

--- popup/popup.js
+++ popup/popup.js
@@ -4,9 +4,25 @@ const form = document.querySelector('#form');
 const list = document.querySelector('.list');
 
 document.addEventListener('DOMContentLoaded', async () => {
+  const isActive = await chrome.storage.local.get().then((item) => item.isActive);
+  if (isActive) {
+    switcher.checked = true;
+  }
   toggleSwitcher(switcher);
 });
-
+function addItem(list, val) {
+  let item = document.createElement('li');
+  item.classList.add('list__item');
+  item.onclick = deleteItem;
+  item.textContent = val;
+  list.append(item);
+}
+async function deleteItem(e) {
+  const blackList = await chrome.storage.sync.get().then((store) => store.blackList);
+  const newList = blackList.filter((item) => item !== e.target.innerText);
+  chrome.storage.sync.set({ blackList: newList });
+  e.target.remove();
+}
 function toggleSwitcher(switcher) {
   switcher.addEventListener('change', (event) => {
     if (event.target.checked) {
