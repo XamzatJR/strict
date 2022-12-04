33ef151d2920653d3c7c7bd9f1a997ec2b03fa8c
XamzatJR <bahaev.hamzat95@gmail.com>
2022-12-04 22:26:13 +0300

    feat: add functions isEmpty, handleURL and validateInput. add event listener on form submit

--- popup/popup.js
+++ popup/popup.js
@@ -2,7 +2,24 @@ const switcher = document.querySelector('#switcher');
 const input = document.querySelector('#input');
 const form = document.querySelector('#form');
 const list = document.querySelector('.list');
-
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const blackList = await chrome.storage.sync.get().then((store) => {
+    if (!isEmpty(store.blackList)) {
+      return store.blackList;
+    }
+    return [];
+  });
+  if (!validateInput(input)) return;
+  if (!blackList.includes(input.value)) {
+    addItem(list, handleURL(input.value));
+    chrome.storage.sync.set({ blackList: [...blackList, input.value] }, () => {
+      input.value = '';
+    });
+  } else {
+    alert('This site has already been added');
+  }
+});
 document.addEventListener('DOMContentLoaded', async () => {
   const isActive = await chrome.storage.local.get().then((item) => item.isActive);
   if (isActive) {
@@ -31,4 +48,31 @@ function toggleSwitcher(switcher) {
       chrome.storage.local.set({ isActive: false });
     }
   });
+}
+function validateInput(input) {
+  const domain = new RegExp(
+    '(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]',
+    'g'
+  );
+  if (!domain.test(input.value)) {
+    alert('not valid URL');
+    return false;
+  }
+  return true;
+}
+function handleURL(url) {
+  let newURL;
+  if (url.startsWith('https://') || url.startsWith('http://')) {
+    newURL = new URL(url);
+  } else {
+    newURL = new URL('https://' + url);
+  }
+  return newURL.host;
+}
+
+function isEmpty(obj) {
+  for (var x in obj) {
+    return false;
+  }
+  return true;
 }
